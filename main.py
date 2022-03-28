14daf1695a6e1d6c9e3a0f6e5ba59a4b72c9a1a4
mdarmstr <mdarmstr@ualbeta.ca>
2022-03-28 11:18:06 -0600

    Initial commit - git test

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 # This is a sample Python script.
+from sklearn.datasets import fetch_covtype
+from sklearn.cross_decomposition import PLSRegression
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+X, y = fetch_covtype(return_X_y=True)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
